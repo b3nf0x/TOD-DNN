14a7f9656f9c478ce993dc5568e2f44b9b1497b2
ben <ben@foxeinstein.com>
2022-12-08 14:22:00 +0100

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -5,7 +5,5 @@ import numpy as np
 
 if __name__ == "__main__":
     for item in os.listdir("dataset/"):
-        print(
-            np.load("dataset/" + item)
-        )
+        print(np.load("dataset/" + item))
         input()

7d0426f43a5a65f4087502f66bd993cda27107ee
ben <ben@foxeinstein.com>
2022-12-08 17:01:24 +0100

    max delta time with argparse

--- generate_synthetic_data_grid.py
+++ generate_synthetic_data_grid.py
@@ -7,15 +7,16 @@ from core.data_model import SynData
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--out_dir", type=str, required=True, default=None, help="dataset save dir path")
+    parser.add_argument("--max_delta_time", type=int, required=False, default=12, help="sets the max delta time")
     args = parser.parse_args()
 
     correction_factors: list = [0.7, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4]
 
     dataset: list = []
-    for mass in tqdm(range(50,122)): # mass
+    for mass in tqdm(range(50, 122)): # mass
         for cf in correction_factors: # each cf
             for ambient_T in range(-10, 37): # ambient temp
-                for tod in range(0,13):
+                for tod in range(0, args.max_delta_time):
                     dataset.append(
                         SynData(
                             mass=mass,

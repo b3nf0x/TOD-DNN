f4c2cd3661f8d0e5e7336520807906c6f624aa25
b3nf0x <102217035+b3nf0x@users.noreply.github.com>
2022-12-09 14:49:41 +0100

    Update generate_synthetic_data_grid.py

--- generate_synthetic_data_grid.py
+++ generate_synthetic_data_grid.py
@@ -15,7 +15,7 @@ if __name__=="__main__":
     dataset: list = []
     for mass in tqdm(range(50, 122)): # mass
         for cf in correction_factors: # each cf
-            for ambient_T in range(-10, 37): # ambient temp
+            for ambient_T in range(-10, 37+1): # ambient temp
                 for tod in range(0, args.max_delta_time+1):
                     dataset.append(
                         SynData(
